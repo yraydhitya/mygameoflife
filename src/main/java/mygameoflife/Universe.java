133d5476a7502790e163dd18cc32e4cb974b6f24
Raydhitya Yoseph <raydhitya.yoseph@go-jek.com>
2021-06-11 22:34:34 +0700

    Override toString in Universe

--- src/main/java/mygameoflife/Universe.java
+++ src/main/java/mygameoflife/Universe.java
@@ -4,6 +4,7 @@ import java.util.Collection;
 
 public class Universe {
     private final Collection<Cell> cells;
+    private final int smallestRow, biggestRow, smallestCol, biggestCol;
 
     public static Universe of(Collection<Cell> cells) {
         return new Universe(cells);
@@ -11,6 +12,10 @@ public class Universe {
 
     private Universe(Collection<Cell> cells) {
         this.cells = cells;
+        this.smallestRow = smallestRow();
+        this.biggestRow = biggestRow();
+        this.smallestCol = smallestCol();
+        this.biggestCol = biggestCol();
     }
 
     public Cell get(int row, int col) {
@@ -18,5 +23,61 @@ public class Universe {
                 .filter(cell -> cell.isPosition(row, col))
                 .findFirst()
                 .orElse(Cell.ofDead(row, col));
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int row = smallestRow; row <= biggestRow; row++) {
+            for (int col = smallestCol; col <= biggestCol; col++) {
+                stringBuilder.append(get(row, col));
+            }
+            stringBuilder.append("\n");
+        }
+        return stringBuilder.toString();
+    }
+
+    private int smallestRow() {
+        Cell firstCell = cells.iterator().next();
+        int smallestRow = firstCell.getRow();
+        for (Cell cell : cells) {
+            if (cell.getRow() < smallestRow) {
+                smallestRow = cell.getRow();
+            }
+        }
+        return smallestRow;
+    }
+
+    private int biggestRow() {
+        Cell firstCell = cells.iterator().next();
+        int biggestRow = firstCell.getRow();
+        for (Cell cell : cells) {
+            if (cell.getRow() > biggestRow) {
+                biggestRow = cell.getRow();
+            }
+        }
+        return biggestRow;
+    }
+
+    private int smallestCol() {
+        Cell firstCell = cells.iterator().next();
+        int smallestCol = firstCell.getCol();
+        for (Cell cell : cells) {
+            if (cell.getCol() < smallestCol) {
+                smallestCol = cell.getRow();
+            }
+        }
+        return smallestCol;
+    }
+
+    private int biggestCol() {
+        Cell firstCell = cells.iterator().next();
+        int biggestCol = firstCell.getCol();
+        for (Cell cell : cells) {
+            if (cell.getCol() > biggestCol) {
+                biggestCol = cell.getCol();
+            }
+        }
+        return biggestCol;
     }
 }
--- src/test/java/mygameoflife/UniverseTest.java
+++ src/test/java/mygameoflife/UniverseTest.java
@@ -28,4 +28,68 @@ public class UniverseTest {
 
         assertEquals(Cell.ofDead(1, 1), universe.get(1, 1));
     }
+
+    @Test
+    public void toStringOneCell() {
+        Universe universe =
+                Universe.of(Set.of(Cell.ofLive(0, 0)));
+
+        assertEquals("X\n", universe.toString());
+    }
+
+    @Test
+    public void toStringOneRow() {
+        Universe universe =
+                Universe.of(Set.of(Cell.ofLive(0, 0), Cell.ofLive(0, 1), Cell.ofLive(0, 2)));
+
+        assertEquals("XXX\n", universe.toString());
+    }
+
+    @Test
+    public void toStringOneRowWithDeadCells() {
+        Universe universe =
+                Universe.of(Set.of(Cell.ofLive(0, 0), Cell.ofLive(0, 1), Cell.ofLive(0, 5)));
+
+        assertEquals("XX   X\n", universe.toString());
+    }
+
+    @Test
+    public void toStringOneCol() {
+        Universe universe =
+                Universe.of(Set.of(Cell.ofLive(0, 0), Cell.ofLive(1, 0), Cell.ofLive(2, 0)));
+
+        assertEquals("X\nX\nX\n", universe.toString());
+    }
+
+    @Test
+    public void toStringOneColWithDeadCells() {
+        Universe universe =
+                Universe.of(Set.of(Cell.ofLive(0, 0), Cell.ofLive(1, 0), Cell.ofLive(5, 0)));
+
+        assertEquals("X\nX\n \n \n \nX\n", universe.toString());
+    }
+
+    @Test
+    public void toStringMatrix() {
+        Universe universe =
+                Universe.of(Set.of(
+                        Cell.ofLive(0, 0), Cell.ofLive(0, 1), Cell.ofLive(0, 2),
+                        Cell.ofLive(1, 0), Cell.ofLive(1, 1), Cell.ofLive(1, 2),
+                        Cell.ofLive(2, 0), Cell.ofLive(2, 1), Cell.ofLive(2, 2)
+                ));
+
+        assertEquals("XXX\nXXX\nXXX\n", universe.toString());
+    }
+
+    @Test
+    public void toStringMatrixWithDeadCells() {
+        Universe universe =
+                Universe.of(Set.of(
+                        Cell.ofLive(0, 0), Cell.ofLive(0, 1),
+                        Cell.ofLive(1, 0), Cell.ofLive(1, 2),
+                        Cell.ofLive(2, 1), Cell.ofLive(2, 2)
+                ));
+
+        assertEquals("XX \nX X\n XX\n", universe.toString());
+    }
 }
